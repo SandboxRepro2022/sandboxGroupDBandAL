a328adee10d3c3e57d3671aad903b7bf36520e20
Ubuntu <ubuntu@machine3ad9a3c2-1854-4d73-9311-ba50e75880a3.oscarlocal>
2022-09-16 14:54:22 +0000

    resolve w+

--- fichier.py
+++ fichier.py
@@ -4,7 +4,7 @@ URL = "https://www.data.gouv.fr/fr/datasets/r/dc7663c7-5da9-4765-a98b-ba4bc9de90
   
 file = requests.get(URL, stream = True)
   
-with open("covid_data.txt","wb") as pdf:
+with open("covid_data.txt","w+") as pdf:
     for chunk in file.iter_content(chunk_size=1024):
   
          if chunk:
